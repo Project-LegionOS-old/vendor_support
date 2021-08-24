AtomXCLO/android_vendor_support

Margaritov: Adapt imports for VendorSupport

src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -30,7 +30,7 @@ import android.view.KeyEvent;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-import com.android.settings.R;
+import com.ssos.support.R;
 
 public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 

src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -38,7 +38,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.settings.R;
+import com.ssos.support.R;
 
 /**
  * A preference type that allows a user to choose a time

Project-LegionOS-old/vendor_support

Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -30,7 +30,7 @@ import android.view.KeyEvent;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-import com.android.settings.R;
+import com.legion.support.R;
 
 public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 

src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -38,7 +38,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.settings.R;
+import com.legion.support.R;
 
 /**
  * A preference type that allows a user to choose a time